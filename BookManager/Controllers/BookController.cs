19634895ba117df7c4b7dfd433fbdd78d3e5ed95
Long Nguyen <hoanglong123000@gmail.com>
2021-06-29 16:35:51 +0700

    Add project files.

---
+++ BookManager/Controllers/BookController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.ModelBinding;
+using BookManager.Models;
+
+
+namespace BookManager.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public String HelloTeacher(string month, string year)
+        {
+            return "HUTECH2021"+ month + "/" + year;
+        }
+
+        public ActionResult ListBook()
+        {
+            List<string> listbook = new List<string>();
+            listbook.Add("Sách 129");
+            listbook.Add("Sách 19");
+            listbook.Add("Sách 13");
+            listbook.Add("Sách 11");
+            listbook.Add("Sách 10");
+
+            ViewBag.Books = listbook;
+
+
+            return View();
+        }
+
+        public ActionResult ListBookModel()
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML", "Ông X", "/Content/images/b1.jpg"));
+            books.Add(new Book(2, "HTML", "Ông Y", "/Content/images/b2.jpg"));
+            books.Add(new Book(3, "HTML", "Ông Z", "/Content/images/b3.jpg"));
+            return View(books);
+        }
+        [HttpPost, ActionName("EditBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditBook(int id, string title, string author, string image_url)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML", "Ông X", "/Content/images/b1.jpg"));
+            books.Add(new Book(2, "HTML", "Ông Y", "/Content/images/b2.jpg"));
+            books.Add(new Book(3, "HTML", "Ông Z", "/Content/images/b3.jpg"));
+
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    b.Title = title;
+                    b.Author = author;
+                    b.Image_url = image_url;
+                    break;
+                }
+            }
+
+            
+            return View("ListBookModel",books);
+
+
+        }
+
+        public ActionResult EditBook(int id)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML", "Ông X", "/Content/images/b1.jpg"));
+            books.Add(new Book(2, "HTML", "Ông Y", "/Content/images/b2.jpg"));
+            books.Add(new Book(3, "HTML", "Ông Z", "/Content/images/b3.jpg"));
+
+            Book book = new Book();
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    book = b;
+                    break;
+                }
+            }
+
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(book);
+
+        }
+
+
+        public ActionResult CreateBook()
+        {
+            return View();
+    }
+        
+        
+
+
+
+    }
+}
---
+++ BookManager/Models/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace BookManager.Models
+{
+    
+    public class Book
+    {
+
+        private int id;
+        private string title;
+        private string author;
+        private string image_url;
+
+
+        public int Id { get => id;
+            set => id = value;
+        }
+        [Required(ErrorMessage = "Tiêu đề không được để trống")]
+        [StringLength(250, ErrorMessage = "Tiêu đề không được vượt quá 250 ký tự")]
+        [Display(Name = "Tiêu đề")]
+        public string Title { get => title; set => title = value; }
+        public string Author { get => author; set => author = value; }
+        public string Image_url { get => image_url; set => image_url = value; }
+
+        public Book()
+        {
+            
+        }
+        public Book(int id, string title, string author, string image_url)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.image_url = image_url;
+        }
+    }
+
+    
+}
